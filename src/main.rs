718046c05681a7b5cd38ee25b1c0833e30675d96
Joe Richey <joerichey@google.com>
2018-10-02 00:55:46 -0700

    VGA Module
    Code modified from:
      https://os.phil-opp.com/vga-text-mode/
      https://os.phil-opp.com/unit-testing/
    
    We can now print out the boot info, panic info, and colored text.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-#![feature(uniform_paths)]
+#![feature(uniform_paths, const_fn)]
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
+mod io;
 mod util;
 
-use bootloader_precompiled::{bootinfo, entry_point};
-use core::panic::PanicInfo;
+use bootloader_precompiled::{bootinfo::BootInfo, entry_point};
+use core::{fmt::Write, ops::Deref, panic::PanicInfo};
+use io::vga::{Color, SCREEN};
 use util::halt;
 
-fn bootloader_main(_info: &'static bootinfo::BootInfo) -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in b"Hello World!".iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+fn bootloader_main(info: &'static BootInfo) -> ! {
+    writeln!(SCREEN.lock(), "P4 = 0x{:x}", info.p4_table_addr);
+    writeln!(SCREEN.lock(), "Memory Map:");
+    for region in info.memory_map.iter() {
+        writeln!(
+            SCREEN.lock(),
+            "    [0x{:012x}, 0x{:012x}) - {:?}",
+            region.range.start_addr(),
+            region.range.end_addr(),
+            region.region_type,
+        );
     }
+    writeln!(SCREEN.lock(), "Package = {:?}", info.package.deref());
+    writeln!(SCREEN.lock());
 
+    SCREEN.lock().set_font_color(Color::Magenta);
+    write!(SCREEN.lock(), "Hello ");
+    SCREEN.lock().set_font_color(Color::LightBlue);
+    write!(SCREEN.lock(), "World!");
+
+    panic!("OOPS");
     halt()
 }
 
@@ -27,6 +40,8 @@ entry_point!(bootloader_main);
 
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    SCREEN.lock().set_font_color(Color::LightRed);
+    writeln!(SCREEN.lock(), "\n\nKERNEL PANIC\n  {}", info);
     halt()
 }
