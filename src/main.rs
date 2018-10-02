3b9f129917e61d40a6bec51ab11fdd01e64d2088
Joe Richey <joerichey@google.com>
2018-10-01 23:00:42 -0700

    Initial Minimal Kernel
    Code after completing: https://os.phil-opp.com/minimal-rust-kernel

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+use core::panic::PanicInfo;
+
+use bootloader_precompiled::{bootinfo, entry_point};
+
+fn bootloader_main(_info: &'static bootinfo::BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in b"Hello World!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[cfg(not(test))]
+entry_point!(bootloader_main);
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
