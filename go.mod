bf1cf176c39fbda642e9880113a4d8c17e5058e3
Joe Richey <joerichey@google.com>
2019-09-04 19:15:16 -0700

    Create Go project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/josephlr/rak
+
+go 1.13
+
+require rsc.io/quote v1.5.2
--- src/io/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod ioapic;
-pub mod lapic;
-pub mod pic8259;
-pub mod ps2;
-pub mod serial;
-pub mod vga;
